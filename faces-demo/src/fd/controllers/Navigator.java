9faffec507c30db7d782cc6c21c5eed4b9fd9397
Duy Phung <duypv98@gmail.com>
2019-07-22 11:23:39 +0700

    Fixed link and Views

--- faces-demo/src/fd/controllers/Navigator.java
+++ faces-demo/src/fd/controllers/Navigator.java
@@ -1,21 +1,28 @@
 package fd.controllers;
 
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 @ManagedBean
 @ViewScoped
-public class Navigator {
+public class Navigator implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4367809453523922642L;
+
 	public String login() {
-		return "login" + "faces-redirect=true";
+		return "login" + "?faces-redirect=true";
 	}
 	
 	public String register() {
-		return "login" + "faces-redirect=true";
+		return "login" + "?faces-redirect=true";
 	}
 	
 	public String home() {
-		return "index" + "faces-redirect=true";
+		return "index" + "?faces-redirect=true";
 	}
 	
 	public String userInfo() {
